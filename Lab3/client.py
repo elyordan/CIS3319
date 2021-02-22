cdb630e0b3108006c530543db875702c5fd74672
Yunior Lopez <yuniorlopez@Yuniors-MacBook-Air.local>
2021-02-22 17:18:35 -0500

    add old lab files

---
+++ Lab3/client.py
@@ -0,0 +1,83 @@
+from Crypto.Cipher import DES
+import socket
+
+#header is the maximum bit number for the message
+HEADER = 64
+PORT = 5050
+#format in which the bits are encoded or decoded 
+FORMAT = 'utf-8'
+#message so the server knows that the client gets disconnected so they can connect again
+DISCONNECT_MESSAGE = "!DISCONNECTED"
+#can use a hard coded ip but because its going to change to another computer is better to get the ip of the server
+#SERVER = "127.0.0.1"
+SERVER = socket.gethostbyname(socket.gethostname())
+#contains the  ip and port
+ADDRESS = (SERVER, PORT)
+
+#client will be equal to a socket stream
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#connect client to the ip and port
+client.connect(ADDRESS)
+
+""" def pad(text):
+    n = len(text) % 8
+    return text + (b' ' * n) """
+
+#check if the msg is 8 bytes long if not them add empty spaces to make it 8
+def pad(text):
+    if len(text) % 8 != 0:
+        toAdd = 8 - len(text) % 8
+    return text + (b' ' * toAdd)
+
+# Function that removes the shared DES Key from the file, and returns it as a string
+def getDESKeyFromFile(filename):
+    DES_key = None
+    with open(filename, "r") as key_file:
+        DES_key = key_file.readline().strip("\r\n")
+
+    print("The shared DES Key is: " + DES_key)
+    return DES_key
+
+#func to encrypt message before sending to server
+
+#func to send a message to the server
+def send(msg):
+    #get the message lenght
+    msg_length = len(msg)
+    #encode message as a string
+    send_length = str(msg_length).encode(FORMAT)
+    #take the message lenght and add it to the in order to make it 64 which is the header and its what the server is whathing for
+    send_length += b' ' * (HEADER - len(send_length))
+    #send the length of the message first
+    client.send(send_length)
+    #send the real messge econd
+    client.send(msg)
+    #receive the message from client
+    serverMsg = client.recv(2048)
+
+    print(f"[ENCRYPTED MESSAGE FROM SERVER] {serverMsg}")
+    print(f"[UNENCRYPTED MESSAGE FROM SERVER] {des.decrypt(serverMsg)}")
+
+
+key = bytes(getDESKeyFromFile('key.txt'), 'utf-8')
+userInput = input()
+text1 = bytes(userInput, 'utf-8')
+
+des = DES.new(key, DES.MODE_ECB)
+
+padded_text = pad(text1)
+encrypted_text = des.encrypt(padded_text)
+
+print("\n")
+
+print(f"[ENCRYPTING MESSAGE] {userInput}")
+print(f"[ENCRYPTED MESSAGE] {encrypted_text}")
+
+print("MESSAGE SENT\n")
+
+send(encrypted_text)
+
+#print(des.decrypt(encrypted_text))
+
+#send a message to disconnect the client
+#send(DISCONNECT_MESSAGE)
---
+++ Lab3/server.py
@@ -0,0 +1,95 @@
+from Crypto.Cipher import DES
+import socket
+import random
+
+#header is the maximum bit number for the message
+HEADER = 64
+PORT = 5050
+#Get the ip address from the local network
+SERVER = socket.gethostbyname(socket.gethostname())
+#contains the  ip and port
+ADDRESS = (SERVER, PORT)
+#format in which the bits are encoded or decoded 
+FORMAT = 'utf-8'
+#message so the server knows that the client gets disconnected so they can connect again
+DISCONNECT_MESSAGE = "!DISCONNECTED"
+#key used to encript messages
+DES_key = str(random.randint(10000000,99999999))
+
+#create a new file with the shared key for the client
+def shareKey():
+    f = open('key.txt', 'w')
+    f.write(DES_key)
+    f.close()  
+
+shareKey()
+
+
+""" def pad(text):
+    n = len(text) % 8
+    return text + (b' ' * n) """
+
+#check if the msg is 8 bytes long if not them add empty spaces to make it 8
+def pad(text):
+    if len(text) % 8 != 0:
+        toAdd = 8 - len(text) % 8
+    return text + (b' ' * toAdd)
+
+#server will be equal to a socket stream
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Bind the server with the address
+server.bind(ADDRESS)
+
+#After receiving the client's info this func makes sure the client is connected, meassure the msg length in contrast to the header size and encodes or decodes the msg using utf-8
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    print("\n")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[ENCRYPTED MESSAGE FROM CLIENT] {msg}")
+
+            key = bytes(DES_key, 'utf-8')
+            #text1 = bytes('HELLO SERVER', 'utf-8')
+
+            des = DES.new(key, DES.MODE_ECB)
+
+            print(f"[UNENCRYPTED MESSAGE FROM CLIENT] {des.decrypt(msg)}")
+            print("\n")
+
+            if connected == False:
+                text1 = bytes('Connection Closed!', 'utf-8')
+                padded_text = pad(text1)
+                encrypted_text = des.encrypt(padded_text)
+                conn.send(encrypted_text)
+            else:
+                text1 = bytes('Hello Client', 'utf-8')
+                padded_text = pad(text1)
+                encrypted_text = des.encrypt(padded_text)
+                print(f"[ENCRYPTING MESSAGE] {'Hello Client'}")
+                print(f"[ENCRYPTED MESSAGE] {encrypted_text}")
+
+                conn.send(encrypted_text)
+                print("MESSAGE SENT\n")
+    #close the connection
+    conn.close()
+
+#start func where the erver listen for a connection then calls the handle_client passing the client's info
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    print("The shared DES Key is: " + DES_key)
+    while True:
+        conn, addr = server.accept()
+        handle_client(conn, addr)
+
+print("[STARTING] Server is starting...")
+
+start()
