4e04ae03465468d040c2a7ade559fa7829f3e087
Yunior Lopez <yuniorlopez@Yuniors-MacBook-Air.local>
2021-02-23 11:02:07 -0500

    modify small error on client side

--- Lab3/client.py
+++ Lab3/client.py
@@ -1,9 +1,11 @@
+from types import resolve_bases
 from Crypto.Cipher import DES
 from Crypto.Hash.MD5 import MD5Hash
 import socket
 import hashlib
 import hmac
 import base64
+import binascii
 
 
 
@@ -34,8 +36,9 @@ client.connect(ADDRESS)
 #check if the msg is 8 bytes long if not them add empty spaces to make it 8
 def pad(text):
     if len(text) % 8 != 0:
-        toAdd = 8 - len(text) % 8
+        toAdd = 8 - len(text) % 8 
     return text + (b' ' * toAdd)
+     
 
 # Function that removes the shared DES Key from the file, and returns it as a string
 def getKeyFromFile(filename):
@@ -68,9 +71,34 @@ def send(msg):
     #receive the message from client
     serverMsg = client.recv(2048)
 
-    print(f"[ENCRYPTED MESSAGE FROM SERVER] {serverMsg}")
-    print(f"[UNENCRYPTED MESSAGE FROM SERVER] {des.decrypt(serverMsg)}")
+    print("******** NEW MESSAGE FROM SERVER ******** ")
+    print("\n")
 
+    #split the ciphertext to only get the hmac key and the encoded text, then decode it
+    string = des.decrypt(serverMsg)
+    n = 32
+    out = [(string[i:i+n]) for i in range(0, len(string), n)] 
+    hexFromServer = out[1].decode()
+    hmacFromServer = out[0].decode()
+    finalmsg = bytes.fromhex(hexFromServer).decode('utf-8')
+
+
+
+    print(f"[MESSAGE] {finalmsg}")
+    print(f"[MESSAGE HMAC] {hexFromServer}")
+    print(f"[HMAC KEY] {hmacFromServer}")
+    print(f"[CIPHERTEXT] {des.decrypt(serverMsg)}")
+    print(f"[ENCRYPTED DES MESSAGE] {serverMsg}")
+
+    if(hmacFromServer == hashmac):
+        print(f"[HMAC FROM SERVER] {hmacFromServer}")
+        print(f"[HMAC GENERATED] {hashmac}")
+        print('HMAC HAS BEEN VERIFIED!')
+    else:
+        print(f"[HMAC FROM CLIENT] {hmacFromServer}")
+        print(f"[HMAC GENERATED] {hashmac}")
+        print('HMAC DO NOT MATCH!')
+                
 
 key = bytes(getKeyFromFile('key.txt'), 'utf-8')
 hmackey = bytes(getKeyFromFile('hmackey.txt'), 'utf-8')
@@ -80,12 +108,13 @@ text1 = bytes(userInput, 'utf-8')
 des = DES.new(key, DES.MODE_ECB)
 
 #encrypt the message using hmac first
-sign_signature = hmac.new(text1, hmackey, hashlib.md5).hexdigest()
+#sign_signature = hmac.new(text1, hmackey, hashlib.md5).hexdigest()
+sign_signature = text1.hex()
 hashmac = hmac.new(hmackey,digestmod=MD5Hash).hexdigest()
 finalmsg = hashmac + '' + sign_signature
 
 
-#padded_text = pad(my_hmac)
+
 encrypted_text = des.encrypt(finalmsg)
 
 print("\n")
